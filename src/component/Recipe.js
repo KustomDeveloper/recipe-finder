877808b128349f19dbd9b0220c10bb77cffa8640
Michael Hicks <kustomdesigner@gmail.com>
2021-12-29 14:14:45 -0600

    Finished videoplayer

--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -82,7 +82,9 @@ const Recipe = () => {
 
             <div className="recipe-title watch-vid-title">Watch the Video:</div>
            
-            <a id="" target="_new" href={videoUrl}><img width="100%" src={recipeImg}></img></a>
+            <a id="videoplayer-link" target="_new" href={videoUrl}><img width="100%" src={recipeImg}></img>
+            <span className="arrow-right-bg"><span className="arrow-right"></span></span>
+            </a>
     
             </div>
         </div>
